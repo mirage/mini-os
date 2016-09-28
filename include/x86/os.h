mirage/mini-os

commit e35295d724d64e6e025ec31c2e510e448b8641d1

mini-os: fix builds with uncommon config settings
Some config settings won't build standalone. Fix the following cases:

- all CONFIG_* set to "n"
- standard config with latest Xen interface version

Signed-off-by: Juergen Gross <jgross@suse.com>
Reviewed-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

include/x86/os.h
@@ -514,6 +514,11 @@ static __inline__ unsigned long __ffs(unsigned long word)
 #endif /* ifdef __INSIDE_MINIOS */
 
 /********************* common i386 and x86_64  ****************************/
+#define xen_mb()  mb()
+#define xen_rmb() rmb()
+#define xen_wmb() wmb()
+#define xen_barrier() asm volatile ( "" : : : "memory")
+
 #define wrmsr(msr,val1,val2) \
       __asm__ __volatile__("wrmsr" \
                            : /* no outputs */ \

include/xenbus.h
@@ -7,10 +7,14 @@ typedef unsigned long xenbus_transaction_t;
 #define XBT_NIL ((xenbus_transaction_t)0)
 
 #ifdef CONFIG_XENBUS
+extern uint32_t xenbus_evtchn;
+
 /* Initialize the XenBus system. */
 void init_xenbus(void);
 void get_xenbus(void *p);
 #else
+#define xenbus_evtchn ~0
+
 static inline void init_xenbus(void)
 {
 }
@@ -32,8 +36,6 @@ struct xenbus_event {
     struct xenbus_event *next;
 };
 typedef struct xenbus_event *xenbus_event_queue;
-
-extern uint32_t xenbus_evtchn;
 
 char *xenbus_watch_path_token(xenbus_transaction_t xbt, const char *path, const char *token, xenbus_event_queue *events);
 char *xenbus_unwatch_path_token(xenbus_transaction_t xbt, const char *path, const char *token);

commit e20998fbec0af4d783abb1a0695ab4614064c520

minios: fix build issue with xen_*mb defines
Commit e35295d724d64e6e025ec31c2e510e448b8641d1 ("mini-os: fix builds
with uncommon config settings") introduced a bug which will break
the build, as xc_minios.c will have defined those macros via
xc_private.h.

Fix this by defining the macros only if compiling a minios source.

Signed-off-by: Juergen Gross <jgross@suse.com>
Reviewed-by: Wei Liu <wei.liu2@citrix.com>
Acked-by: Samuel Thibault <samuel.thibault@ens-lyon.org>

include/x86/os.h
@@ -511,13 +511,13 @@ static __inline__ unsigned long __ffs(unsigned long word)
 #error "Unsupported architecture"
 #endif
 
-#endif /* ifdef __INSIDE_MINIOS */
-
 /********************* common i386 and x86_64  ****************************/
 #define xen_mb()  mb()
 #define xen_rmb() rmb()
 #define xen_wmb() wmb()
 #define xen_barrier() asm volatile ( "" : : : "memory")
+
+#endif /* ifdef __INSIDE_MINIOS */
 
 #define wrmsr(msr,val1,val2) \
       __asm__ __volatile__("wrmsr" \